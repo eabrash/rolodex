6c3ff2a72a6a00bd5b7196f13eca0b412fd4690f
Emily Abrash <emabrash@gmail.com>
2016-11-30 09:17:58 -0800

    Went back and did wave 1 by itself (displaying just one contact directly from app.js)

--- src/app.js
+++ src/app.js
@@ -3,21 +3,23 @@ import ApplicationView from 'app/views/application_view';
 import Contact from 'app/models/contact';
 import Rolodex from 'app/collections/rolodex';
 import RolodexView from 'app/views/rolodex_view';
+import ContactView from 'app/views/contact_view';
 import $ from 'jquery';
-
+import _ from 'underscore';
 
 
 var contactData = [{name: "Satine the Cat", phone: "425 785 9393", email: "satine@himalayan.com"}];
 
 $(document).ready(function(){
 
-  var application = new Application({rolodex: contactData});
+  var contact = new Contact(contactData[0]);
+  var contactTemplate = _.template($('#tmpl-contact-card').html());
+  // this.detailedTemplate = _.template($('#tmpl-contact-details').html());
 
-  var appView = new ApplicationView({
-    el: 'html',
-    model: application
-  });
+  var contactView = new ContactView({model: contact, template: contactTemplate});
 
-  appView.render();
+  console.log(contactView.render().$el);
+
+  $("#contact-cards").append(contactView.render().$el);
 
 })
